fb759194d056f17b6d95cbe01853d79bcf834e12
Gionnelly Vielza Durán <80008338+gvielza@users.noreply.github.com>
2024-05-08 00:36:20 +0000

    add class Automovil

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-mensaje="probandogit"
-print(mensaje)
+class Automovil:
+  ruedas=4
+  def __init__(self,color, marca, aceleracion, velocidad):
+    self.color=color
+    self.marca=marca
+    self.aceleracion=aceleracion
+    self.velocidad=velocidad
+
+
+
+  
